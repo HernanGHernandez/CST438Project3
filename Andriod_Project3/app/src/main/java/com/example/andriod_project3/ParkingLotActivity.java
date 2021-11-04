0094ceb6f6525ba3266aacb70ce4b6ce8d06a7f8
arsalaerfan <erfanarsala831@gmail.com>
2021-11-04 10:38:28 -0700

    added files

---
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/ParkingLotActivity.java
@@ -0,0 +1,14 @@
+package com.example.andriod_project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParkingLotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parking_lot);
+    }
+}
---
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.andriod_project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
