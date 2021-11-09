b8b58509e9002f2669fda95d4cfc726e5cf236a0
Oscar Ruiz <Oscarruiz25341@gmail.com>
2021-11-08 16:32:38 -0800

    tweaking and adding layout

--- Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
@@ -3,9 +3,14 @@ package com.example.andriod_project3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class create_account extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
