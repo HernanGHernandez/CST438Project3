7959264897279792344ccf08ea1083041831b406
Oscar Ruiz <Oscarruiz25341@gmail.com>
2021-11-14 21:26:58 -0800

    Update create_account.java

--- Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
@@ -3,12 +3,26 @@ package com.example.andriod_project3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class create_account extends AppCompatActivity {
-
+EditText name;
+EditText pass;
+Button create;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        name = findViewById(R.id.User);
+        pass = findViewById(R.id.password);
+        create = findViewById(R.id.button);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
