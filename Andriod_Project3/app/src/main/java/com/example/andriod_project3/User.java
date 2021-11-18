5789e108ad2ddbdeb09c69a756a142a63183ab26
arsalaerfan <erfanarsala831@gmail.com>
2021-11-18 09:54:29 -0800

    Model classes for parking, user and messages

---
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/User.java
@@ -0,0 +1,42 @@
+package com.example.andriod_project3;
+import com.google.gson.annotations.SerializedName;
+
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+
+    @SerializedName("body")
+    private String text;
+
+    public User(int userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
