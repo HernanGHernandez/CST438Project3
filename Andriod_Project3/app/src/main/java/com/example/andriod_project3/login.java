a26f4e24c59d1336690e75c616bc4c36e8c589de
arsalaerfan <erfanarsala831@gmail.com>
2021-11-18 09:09:17 -0800

    update info

--- Andriod_Project3/app/src/main/java/com/example/andriod_project3/login.java
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/login.java
@@ -9,8 +9,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
-    private EditText name, pass;
-    private Button loginBtn,regBtn;
+    private EditText name;
+    private EditText pass;
+    private Button loginBtn;
+    private Button regBtn;
     String user = "123";
     String password = "321";
 
@@ -21,7 +23,6 @@ public class login extends AppCompatActivity {
 
         name = findViewById(R.id.username);
         pass = findViewById(R.id.password);
-
         loginBtn = findViewById(R.id.loginBtn);
         regBtn = findViewById(R.id.regBtn);
 
@@ -33,18 +34,21 @@ public class login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // doesnt work the pop up message aint doing its job
                 String userField = name.getText().toString();
                 String passField = pass.getText().toString();
+                Toast.makeText(login.this, "test", Toast.LENGTH_LONG);
 
-                if (userField.isEmpty() && passField.isEmpty()) {
+                if (userField.isEmpty() || passField.isEmpty()) {
                     Toast.makeText(login.this, "Username/Password input missing", Toast.LENGTH_LONG);
                 }
                 else if(userField == user && passField == password)
                 {
+                    Toast.makeText(login.this, "Successful login", Toast.LENGTH_LONG);
                     Intent intent = new Intent(login.this, MainActivity.class);
                     startActivity(intent);
                 }
