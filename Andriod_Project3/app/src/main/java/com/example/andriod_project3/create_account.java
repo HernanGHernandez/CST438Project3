bb68bdb5a42c6e945b6a1756439562e32b6881ef
Oscar Ruiz <Oscarruiz25341@gmail.com>
2021-11-19 19:48:04 -0800

    tweaks, and adding retrofit

--- Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
+++ Andriod_Project3/app/src/main/java/com/example/andriod_project3/create_account.java
@@ -2,13 +2,38 @@ package com.example.andriod_project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class create_account extends AppCompatActivity {
+    private Button backk;
+    private Button create;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        username = findViewById(R.id.User);
+        password = findViewById(R.id.create_password);
+        create = findViewById(R.id.button);
+        backk = findViewById(R.id.backk);
+        backk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(create_account.this, login.class);
+                startActivity(intent);
+            }
+        });
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
