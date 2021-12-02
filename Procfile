f3acae6f223099fa928ca759edc955e7b542bce4
HernanHernandez <67992122+HernanGHernandez@users.noreply.github.com>
2021-12-02 15:33:04 -0800

    heroku update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn prhome.wsgi
--- prhome/urls.py
+++ prhome/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
-    path('ParkResApp/', include('ParkResApp.urls'))
+    #path('', views.home, name="home"),
+    path('', include('ParkResApp.urls'))
+    #path('ParkResApp/', include('ParkResApp.urls'))
 ]
